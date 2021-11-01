30d0d294d46032c6c5dcded3e3bb0aeeb81471c7
John Hottinger <johnthottinger@gmail.com>
2021-10-31 20:57:28 -0700

    API v2

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -6,13 +6,14 @@
 
 /* eslint-disable no-console */
 
-import jsf, { generate } from 'json-schema-faker';
+import {generate,extend} from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk'; //color output
 
 //Extend JSF with the fake libs you want to use.
 
+extend("faker", () => require("faker"));
 const json = JSON.stringify(generate(schema));
 
 fs.writeFile("./src/api/db.json", json, function (err) {
